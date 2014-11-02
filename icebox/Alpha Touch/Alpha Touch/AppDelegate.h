b0ec7f2e08c6a6e8207869ec35c832bd6cfcef1c
toannguyenle <toan@me.com>
2014-11-02 08:09:16 -0800

    Initial Commit

---
+++ icebox/Alpha Touch/Alpha Touch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alpha Touch
+//
+//  Created by Toan Nguyen Le on 11/2/14.
+//  Copyright (c) 2014 Toan Nguyen Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ icebox/Alpha Touch/Alpha Touch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Alpha Touch
+//
+//  Created by Toan Nguyen Le on 11/2/14.
+//  Copyright (c) 2014 Toan Nguyen Le. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
