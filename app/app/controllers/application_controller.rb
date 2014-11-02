ab8509d2a12fe675fbcc5ba7f9d88c3dace045d4
toannguyenle <toan@me.com>
2014-11-01 21:25:04 -0700

    Update background image

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  config.i18n.enforce_available_locales = true
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/config/initializers/cookies_serializer.rb
+++ app/config/initializers/cookies_serializer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-# Rails.application.config.action_dispatch.cookies_serializer = :json
+Rails.application.config.action_dispatch.cookies_serializer = :json
 # Try to see if this work
-YourApp::Application.config.session_store :cookie_store, key: '_yourapp_session'
+# YourApp::Application.config.session_store :cookie_store, key: '_yourapp_session'
